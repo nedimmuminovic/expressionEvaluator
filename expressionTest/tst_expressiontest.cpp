f7beab98bcd92459717b12cdb1a665396293a9e7
nedimmuminovic <nediim@windowslive.com>
2013-07-23 13:01:10 +0200

    Version 0.2.3
    - more test cases added

--- expressionTest/tst_expressiontest.cpp
+++ expressionTest/tst_expressiontest.cpp
@@ -1,7 +1,8 @@
 #include <QString>
 #include <QtTest>
-#include "../expressionEvaluator/expevaluator.cpp"
 #include "../expressionEvaluator/lexer.cpp"
+#include "../expressionEvaluator/expEvaluator.cpp"
+
 
 class ExpressionTest : public QObject
 {
@@ -11,7 +12,9 @@ public:
     ExpressionTest();
     
 private Q_SLOTS:
+     void storeLexems(Lexer &lexer, expEvaluator &expEval);
     void testCase1();
+
 };
 
 ExpressionTest::ExpressionTest()
@@ -19,22 +22,65 @@ ExpressionTest::ExpressionTest()
 
 }
 
+void ExpressionTest::storeLexems(Lexer &lexer,expEvaluator &expEval){
+
+ QString lexem;
+while(lexer.hasMoreLexems()) {
+
+     lexem=lexer.nextLexem();
+     expEval.setLexems(lexem);
+
+
+} //end of while
+
+}
+
 void ExpressionTest::testCase1()
 {
-    QString lexem;
 
-    Lexer lexer("1.5-0.5");
-    expEvaluator expEval;
+    Lexer *lexer=new Lexer("1.5-0.5");
+    expEvaluator *expEval=new expEvaluator;
+    storeLexems(*lexer,*expEval);
+    QVERIFY(expEval->evaluateExpression()==1.0);
+    delete lexer;
 
-   while(lexer.hasMoreLexems()) {
+    lexer=new Lexer("6");
+    storeLexems(*lexer,*expEval);
+    QVERIFY(expEval->evaluateExpression()==6.0);
+    delete lexer;
 
-        lexem=lexer.nextLexem();
-        expEval.setLexems(lexem);
+    lexer=new Lexer("6+3-1/2*2^2");
+    storeLexems(*lexer,*expEval);
+    QVERIFY(expEval->evaluateExpression()==7.0);
+    delete lexer;
 
+    lexer=new Lexer("-1-2-3");
+    storeLexems(*lexer,*expEval);
+    QVERIFY(expEval->evaluateExpression()==-6.0);
+    delete lexer;
 
-   } //end of while
+    lexer=new Lexer("sqrt(9)+sqrt(10)");
+    storeLexems(*lexer,*expEval);
+    QVERIFY(expEval->evaluateExpression()==6.16228);
+    delete lexer;
 
-    QVERIFY(expEval.evaluateExpression()==1.0);
+    lexer=new Lexer("1.5+2.5/3*6-6");
+    storeLexems(*lexer,*expEval);
+    QVERIFY(expEval->evaluateExpression()==0.5);
+    delete lexer;
+
+    lexer=new Lexer("8-(-3-4)");
+    storeLexems(*lexer,*expEval);
+    QVERIFY(expEval->evaluateExpression()==15);
+    delete lexer;
+
+    lexer=new Lexer("15*(-2)");
+    storeLexems(*lexer,*expEval);
+    QVERIFY(expEval->evaluateExpression()==-30);
+    delete lexer;
+
+    delete expEval;
+
 
 }
 
